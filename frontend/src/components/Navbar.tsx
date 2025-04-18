565b112634d5b89a5ff08a1ea112135c3fb8c5d2
johnsonnguyen05 <johnsonnguyen05@gmail.com>
2025-04-18 14:47:23 -0400

    updated readme, created donation page

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -18,6 +18,12 @@ function Navbar() {
           >
             Profile
           </Link>
+          <Link
+            to="/Donation"
+            className="bg-transparent border border-white text-white font-semibold rounded-md px-4 py-2 text-center cursor-pointer hover:bg-white hover:text-[#1a1a1a] transition-colors"
+          >
+            Donation
+          </Link>
         </div>
       </div>
     </div>
